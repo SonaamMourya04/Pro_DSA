e1d88c3f363b7334e31d99741adbf0ec737e06cb
SonaamMourya04 <sonammourya7683@gmail.com>
2025-09-14 22:54:16 +0530

    commit

--- HASHING/Fundamental.cpp
+++ HASHING/Fundamental.cpp
@@ -1,38 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
- void hasing(int q,  vector<int>&arr,int n){ 
-    while (q--) {
-        int x;
-        cin >> x;  // the query element
-        int count = 0;
-        
-        // Brute-force traversal of array
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == x) {
-                count++;
-            }
-        }
-        
-        cout << count << endl;  // print frequency of x
-    }}
 
-int main() {g
+void hasing(vector<int>& nums, int n, int q) {
+    while (q--) {
+        int x, cnt = 0;
+        cin >> x;
+        for (int i = 0; i < n; i++) {
+            if (x == nums[i]) cnt++;
+        }
+        cout << cnt << "\n";   // newline after each query
+    }
+}
+
+int main() {
     int n;
-    cin >> n;  // size of array
+    cin >> n;
     
-    vector<int> arr(n);
+    vector<int> nums(n);   // ✅ allocate size
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        cin >> nums[i];
     }
     
     int q;
-    cin >> q;  // number of queries
-    hasing(q,arr,n);
+    cin >> q;
     
-   
-    
+    hasing(nums, n, q);
     return 0;
-}  
+}
+
 // git remote add origin https://github.com/SonaamMourya04/Pro_DSA.git
 // git branch -M main
 // git push -u origin main 
